6405a8b15ee1c490cd14cd4af36f27a0b24a4d90
CristianoAlberto <cristianoalberto908347@gmail.com>
2024-03-09 16:35:59 +0100

    feat:Ajustando na visao mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatIconModule } from '@angular/material/icon';
+import { WebcamModule } from 'ngx-webcam';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { MainViewComponent } from './main-view/main-view.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    MatIconModule
+    MatIconModule,
+    WebcamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
   styleUrl: './main-view.component.scss'
 })
-export class MainViewComponent {
-
-}
+export class MainViewComponent { }

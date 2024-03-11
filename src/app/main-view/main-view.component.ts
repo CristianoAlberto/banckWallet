5acd03490e6177d4fea536fc81dc58c58148cfb1
CristianoAlberto <cristianoalberto908347@gmail.com>
2024-03-11 01:40:28 +0100

    feat:Adcionando cartoes e validaçao quando for para adcionar cartao novo

--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -6,8 +6,14 @@ import { Router } from '@angular/router';
   styleUrl: './main-view.component.scss'
 })
 export class MainViewComponent {
+  emptyCardState: boolean = false;
   constructor(private router: Router) { }
   goToCamara() {
     this.router.navigate(['/camara']);
+  }
+  checkEmptyCard(): void {
+    const activeSlide = document.querySelector('.carousel-item.active');
+    const emptyCard = activeSlide?.querySelector('.emptyCard');
+    this.emptyCardState = emptyCard ? true : false;
   }
 }

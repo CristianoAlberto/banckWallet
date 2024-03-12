ece98fb35b0ab1aa2adebde5b90f45ab2ab80cae
CristianoAlberto <cristianoalberto908347@gmail.com>
2024-03-12 17:47:47 +0100

    feat: Ajustes

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './activity.component.scss'
 })
 export class ActivityComponent {
+  historicoSelected: boolean = true;
+  pendentesSelected: boolean = false;
 
+  selectButton(buttonType: string) {
+    if (buttonType === 'historico') {
+      this.historicoSelected = true;
+      this.pendentesSelected = false;
+    } else if (buttonType === 'pendentes') {
+      this.historicoSelected = false;
+      this.pendentesSelected = true;
+    }
+  }
 }

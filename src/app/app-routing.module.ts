985031216e95391a8ee179bfe60bd11bc9cdb630
CristianoAlberto <cristianoalberto908347@gmail.com>
2024-03-20 18:02:48 +0100

    feat:AJustes e testando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,9 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
 import { MainViewComponent } from './main-view/main-view.component';
 import { CamaraComponent } from './camara/camara.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { DefinitionsComponent } from './definitions/definitions.component';
-import { MoreComponent } from './more/more.component';
 import { InformationComponent } from './information/information.component';
 import { ActivityComponent } from './activity/activity.component';
-import { CardsCarouselComponent } from './cards-carousel/cards-carousel.component';
 
 const routes: Routes = [
   {
